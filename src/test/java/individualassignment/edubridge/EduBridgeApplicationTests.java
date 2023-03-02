ee3ec8c45c290acb493a4b5732cd91749d278565
Sobhi Kadi <81371673+sobhikadi@users.noreply.github.com>
2023-03-02 11:08:36 +0100

    initial commit

---
+++ src/test/java/individualassignment/edubridge/EduBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package individualassignment.edubridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
