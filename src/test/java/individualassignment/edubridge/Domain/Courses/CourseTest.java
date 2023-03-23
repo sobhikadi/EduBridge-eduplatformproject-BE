3471b803f82f48a7419d1f64fe69e0b6c1cc49c0
Sobhi Kadi <81371673+sobhikadi@users.noreply.github.com>
2023-03-23 18:57:48 +0100

    fix course tests

--- src/test/java/individualassignment/edubridge/Domain/Courses/CourseTest.java
+++ src/test/java/individualassignment/edubridge/Domain/Courses/CourseTest.java
@@ -2,16 +2,20 @@ package individualassignment.edubridge.Domain.Courses;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CourseTest {
+
     @Test
     void toString_ShouldReturnCourseInfoAsString(){
         Course course = createCourseTestRequest();
 
         String actual = course.toString();
 
-        String expected = "Course(id=1, title=Java For Beginners, description=This course is for beginners in Java and want to start their career in it, provider=John Doe)";
+        String expected = String.format("Course(id=1, title=Java For Beginners, description=This course is for beginners in Java and want to start their career in it, provider=John Doe, creationDate=%s, publishDate=Optional[%s], lastModified=null)", LocalDate.now(), LocalDate.now());
         assertEquals(expected, actual);
 
     }
@@ -19,7 +23,14 @@ class CourseTest {
     @Test
     void equals_ShouldReturnTrue_whenHaveSameValues(){
         Course courseOne = createCourseTestRequest();
-        Course courseTwo = createCourseTestRequest();
+        Course courseTwo = Course.builder()
+                .id(1L)
+                .title("Java For Beginners")
+                .description("This course is for beginners in Java and want to start their career in it")
+                .provider("John Doe")
+                .creationDate(LocalDate.now())
+                .publishDate(Optional.ofNullable(LocalDate.now()))
+                .build();
 
         assertEquals(courseOne, courseTwo);
     }
@@ -31,7 +42,9 @@ class CourseTest {
                 .title("Beginners")
                 .description("This course is for beginners in Java and want to start their career in it")
                 .provider("John Doe")
-                .build();;
+                .creationDate(LocalDate.now())
+                .publishDate(Optional.ofNullable(LocalDate.now()))
+                .build();
         Course courseTwo = createCourseTestRequest();
         assertNotEquals(courseOne, courseTwo);
     }
@@ -45,6 +58,8 @@ class CourseTest {
                 .title("Java For Beginners")
                 .description("This course is for beginners in Java and want to start their career in it")
                 .provider("John Doe")
+                .creationDate(LocalDate.now())
+                .publishDate(Optional.ofNullable(LocalDate.now()))
                 .build();
     }
 
